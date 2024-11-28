fda0c6724d7044298ba0b8bc7634fa71f7c18e56
Razaul007 <raselharuyamare@gmail.com>
2024-11-29 01:03:12 +0600

    added modal

--- main.js
+++ main.js
@@ -10,7 +10,22 @@ const updateNavbarBalance = (amountBalance)=>{
   const remainingBalance = navbarBalance - amountBalance;
   document.getElementById("balance").innerText = remainingBalance.toFixed(2);
   
+};
+
+const addToDonationHistory = (amountBalance, cardTitle) =>{
+   const historyContainer= document.getElementById("history-container");
+
+   const historyEntry = document.createElement("div");
+   historyEntry.classList.add("border", "p-4", "rounded-lg", "shadow-sm");
+   historyEntry.innerHTML =`
+     <h3 class="font-semibold">Title: ${cardTitle}</h3>
+     <p>Donation Amount: ${amountBalance}</p>
+     <p>${new Date().toLocaleString()}</p>
+     `
+   historyContainer.appendChild(historyEntry);
 }
+
+
 
 
 const handleDonate = (button)=>{
@@ -26,9 +41,13 @@ const handleDonate = (button)=>{
   const cardBalance = parseFloat(cardElement.querySelector(".card-balance").innerText);
   console.log(cardBalance);
   const newCardBalance = cardBalance + amountBalance;
+ cardElement.querySelector(".card-balance").innerText = newCardBalance.toFixed(2);
 
-  cardElement.querySelector(".card-balance").innerText = newCardBalance.toFixed(2);
+ const cardTitle = cardElement.querySelector(".card-title").innerText;
+ addToDonationHistory(amountBalance, cardTitle);
+
   cardElement.querySelector(".input").value="";
+  document.getElementById("show-modal-btn").showModal();
 }
 
 
